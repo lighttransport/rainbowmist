5585d4acc80cf28ab47d0744255bf8fee84c19fd
Syoyo Fujita <syoyo@lighttransport.com>
2017-08-15 16:20:45 +0900

    Disable using GLM on CUDA since it requires C++ STL header which cannot be found when compiling CUDA code using NVRTC.

--- rainbowmist.h
+++ rainbowmist.h
@@ -33,6 +33,9 @@ THE SOFTWARE.
 
 #define RAINBOWMIST_CUDA (1)
 
+#define RAINBOWMIST_USE_GLM (0)
+#if RAINBOWMIST_USE_GLM
+
 // Use glm for vector swizzle.
 #ifndef GLM_FORCE_SWIZZLE
 #define GLM_FORCE_SWIZZLE
@@ -42,9 +45,34 @@ THE SOFTWARE.
 #define GLM_FORCE_CUDA
 #endif
 
-#include "glm/glm.hpp"
+#include "glm/vec2.hpp"
+#include "glm/vec3.hpp"
+#include "glm/vec4.hpp"
+#include "glm/geometric.hpp"
 
 using namespace glm;
+
+// From CUDA SDK samples
+#include "helper_math.h"
+
+#define vnormalize(x) normalize(x)
+#define vcross(a, b) cross(a, b)
+#define vdot(a, b) dot(a, b)
+
+#else // !RAINBOWMIST_USE_GLM
+
+// Include helper_math firstly
+#include "helper_math.h"
+
+typedef float2 vec2;
+typedef float3 vec3;
+typedef float4 vec4;
+
+#define vnormalize(x) normalize(x)
+#define vcross(a, b) cross(a, b)
+#define vdot(a, b) dot(a, b)
+
+#endif // RAINBOWMIST_USE_GLM
 
 //#include <float.h> // import FLT_EPSILON
 
@@ -52,32 +80,38 @@ using namespace glm;
 #define FLT_EPSILON (0x1.0p-23f)
 #endif
 
-// From CUDA SDK samples
-#include "helper_math.h"
-
 #define DEVICE __device__
 #define HOST   __host__
 #define GLOBAL 
 #define LOCAL 
 #define KERNEL extern "C" __global__
 
-#define vnormalize(x) normalize(x)
-#define vcross(a, b) cross(a, b)
-#define vdot(a, b) dot(a, b)
 
 static inline vec2 make_vec2(float a, float b)
 {
-  return vec2(a, b);
+  vec2 ret;
+  ret.x = a;
+  ret.y = b;
+  return ret;
 }
 
 static inline vec3 make_vec3(float a, float b, float c)
 {
-  return vec3(a, b, c);
+  vec3 ret;
+  ret.x = a;
+  ret.y = b;
+  ret.z = c;
+  return ret;
 }
 
 static inline vec4 make_vec4(float a, float b, float c, float d)
 {
-  return vec4(a, b, c, d);
+  vec4 ret;
+  ret.x = a;
+  ret.y = b;
+  ret.z = c;
+  ret.w = d;
+  return ret;
 }
 
 #elif defined(OPENCL)  // NOTE(LTE): Application must pass `-D OPENCL` as a OpenCL compile flags.
